29fba687a14bfe8811c17be68a2c8885cb5623ae
jose gomez <josegomez120@gmail.com>
2024-08-30 13:08:15 -0500

    websocket server

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -33,7 +34,11 @@ import { AuthModule } from './auth/auth.module';
 
     FilesModule,
 
-    AuthModule
+    AuthModule,
+
+    MessagesWsModule,
+
+
   ],
   controllers: [],
   providers: [],
